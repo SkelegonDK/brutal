a80889983c32240027210b09eb080a3d4361c2fc
Manuel Thomsen <masotoma@hotmail.com>
2023-12-31 13:00:17 +0100

    Update site URL in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site: 'https://SkelegonDK.github.io/brutal',
+  site: 'https://SkelegonDK.github.io',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   base: '/',
