1921eb1a47f22203deb5d5739d501884f1a372e5
Manuel Thomsen <masotoma@hotmail.com>
2024-10-05 23:21:39 +0200

    updated deploy and path errors

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,10 @@ import sitemap from '@astrojs/sitemap';
 import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
-  // used to generate images
-  site: 'https://skelegondk.github.io/brutal',
+  site: 'https://skelegondk.github.io',
+  base: '/brutal',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
-  base: '/',
   vite: {
     optimizeDeps: {
       exclude: ['@resvg/resvg-js'],
